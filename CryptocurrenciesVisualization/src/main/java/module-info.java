71d2527bb45bc558260ef2f4e95501f0cc24300a
Maria Kałuska <01151384@pw.edu.pl>
2020-12-13 12:05:52 +0000

    Merged in master (pull request #1)
    Master
    
    * Merge remote-tracking branch 'origin/master'
    
    * project structure, first view
    
    * project structure, first view
    
    * CryptocurrenciesVisualization/src/main/resources/org/openjx/secondary.fxml edited online with Bitbucket
    * CryptocurrenciesVisualization/src/main/resources/org/openjx/primary.fxml edited online with Bitbucket
    * CryptocurrenciesVisualization/src/main/java/org/openjx/App.java edited online with Bitbucket
    * CryptocurrenciesVisualization/src/main/java/org/openjx/SecondaryController.java edited online with Bitbucket
    * CryptocurrenciesVisualization/src/main/java/org/openjx/PrimaryController.java edited online with Bitbucket
    * CryptocurrenciesVisualization/src/main/java/module-info.java edited online with Bitbucket

--- CryptocurrenciesVisualization/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.openjx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.openjx to javafx.fxml;
-    exports org.openjx;
-}
---
+++ CryptocurrenciesVisualization/src/main/java/org/crypto/gui/objects/CryptoCurrency.java
@@ -0,0 +1,18 @@
+package org.crypto.gui.objects;
+
+public class CryptoCurrency {
+
+    private String name;
+
+    public CryptoCurrency(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
