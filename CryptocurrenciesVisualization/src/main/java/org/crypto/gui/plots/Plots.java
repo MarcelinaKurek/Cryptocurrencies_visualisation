d881318c333c52c4d338231e27501fe1d993e6f2
Marcelina <01151421@pw.edu.pl>
2021-01-22 22:26:01 +0100

    final plots

--- CryptocurrenciesVisualization/src/main/java/org/crypto/gui/plots/Plots.java
+++ CryptocurrenciesVisualization/src/main/java/org/crypto/gui/plots/Plots.java
@@ -1,44 +1,45 @@
 package org.crypto.gui.plots;
-
 import org.crypto.services.APIClient;
 import tech.tablesaw.api.*;
-import tech.tablesaw.columns.Column;
 import tech.tablesaw.plotly.Plot;
-import tech.tablesaw.plotly.api.BoxPlot;
-import tech.tablesaw.plotly.api.CandlestickPlot;
-import tech.tablesaw.plotly.api.LinePlot;
-import tech.tablesaw.plotly.api.TimeSeriesPlot;
+import tech.tablesaw.plotly.api.*;
+import tech.tablesaw.plotly.components.*;
+import tech.tablesaw.plotly.components.change.Decreasing;
+import tech.tablesaw.plotly.components.change.Increasing;
+import tech.tablesaw.plotly.traces.ScatterTrace;
+import tech.tablesaw.plotly.traces.Trace;
 
-import java.text.ParseException;
+import static tech.tablesaw.aggregate.AggregateFunctions.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
 
+
 public class Plots {
 
-    public LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
+    public static LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
         return dateToConvert.toInstant()
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime();
     }
 
-    public LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+    public static LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
         return dateToConvert.toInstant()
                 .atZone(ZoneId.systemDefault())
                 .toLocalDate();
     }
 
-    public void ExchangeRatePlot(String id, String currency, int days ){
-        APIClient api = new APIClient();
-        Plots plots = new Plots();
-        Date[] dates = api.getMarketChart(id, currency, days).keySet().toArray(new Date[0]);
-        Double[] values = api.getMarketChart(id, currency, days).values().toArray(new Double[0]);
+    public static void ExchangeRatePlot(String id, String currency, int days ){
+        Date[] dates = APIClient.getMarketChart(id, currency, days).keySet().toArray(new Date[0]);
+        Double[] values = APIClient.getMarketChart(id, currency, days).values().toArray(new Double[0]);
 
         LocalDateTime[] datetimes = new LocalDateTime[dates.length];
         int i = 0;
         for (Date t : dates){
-            LocalDateTime dt = plots.convertToLocalDateTimeViaInstant(t);
+            LocalDateTime dt = convertToLocalDateTimeViaInstant(t);
             datetimes[i] = dt;
             i++;
         }
@@ -48,22 +49,21 @@ public class Plots {
         DoubleColumn y = DoubleColumn.create(yaxis, values);
         Table t = Table.create("Exchange Rate");
         t.addColumns(x1, y);
-        Plot.show(TimeSeriesPlot.create("Exchange Rate", t, "date", yaxis));
+        String title = id.toUpperCase() + " exchange rate growth";
+        Plot.show(TimeSeriesPlot.create(title, t, "date", yaxis));
 
     }
 
-    public void ExchangeRateDiffPlot(String id, String currency, int days){
-        APIClient api = new APIClient();
-        Plots plots = new Plots();
-        Date[] dates = api.getMarketChart(id, currency, days).keySet().toArray(new Date[0]);
-        Double[] values = api.getMarketChart(id, currency, days).values().toArray(new Double[0]);
+    public static void ExchangeRateDiffPlot(String id, String currency, int days){
+        Date[] dates = APIClient.getMarketChart(id, currency, days).keySet().toArray(new Date[0]);
+        Double[] values = APIClient.getMarketChart(id, currency, days).values().toArray(new Double[0]);
 
-        double change = 0;
+
         Double[] ychange = new Double[dates.length];
         LocalDateTime[] datetimes = new LocalDateTime[dates.length];
         ychange[0] = 0.0;
         for (int i = 1; i < dates.length; i++){
-            LocalDateTime dt = plots.convertToLocalDateTimeViaInstant(dates[i]);
+            LocalDateTime dt = convertToLocalDateTimeViaInstant(dates[i]);
             datetimes[i] = dt;
             ychange[i] = values[i] - values[i -1];
         }
@@ -72,22 +72,13 @@ public class Plots {
         DoubleColumn y3 = DoubleColumn.create("values", ychange);
         Table t3 = Table.create("Exchange Rate Growth");
         t3.addColumns(x1, y3);
-        Plot.show(TimeSeriesPlot.create("Exchange Rate Growth", t3, "date", "values"));
+        String title = id.toUpperCase() + " exchange rate growth";
+        Plot.show(TimeSeriesPlot.create(title, t3, "date", "values"));
     }
 
-    public void CandleStickPlot(String id, String currency, int days){
-        APIClient api = new APIClient();
-        Plots plots = new Plots();
-        Map<Date, List<Double>> data = api.getOHLC(id, currency, days);
+    public static void CandleStickPlot(String id, String currency, int days){
+        Map<Date, List<Double>> data = APIClient.getOHLC(id, currency, days);
         Date[] dates = data.keySet().toArray(new Date[0]);
-
-        LocalDate[] dt = new LocalDate[dates.length];
-        int j = 0;
-        for (Date t2 : dates){
-            LocalDate dt2 = plots.convertToLocalDateViaInstant(t2);
-            dt[j] = dt2;
-            j++;
-        }
 
         Double[] open = new Double[data.values().size()];
         Double[] high = new Double[data.values().size()];
@@ -102,25 +93,74 @@ public class Plots {
             k++;
         }
 
-        DateColumn x2 = DateColumn.create("date",  dt);
+        DateTimeColumn x1 = null;
+        DateColumn x2 = null;
+        if (days == 1){
+            LocalDateTime[] datetimes = new LocalDateTime[dates.length];
+            for (int i = 1; i < dates.length; i++){
+                LocalDateTime ldt = convertToLocalDateTimeViaInstant(dates[i]);
+                datetimes[i] = ldt;
+            }
+            x1 = DateTimeColumn.create("date", datetimes);
+        }
+        else{
+            LocalDate[] datatime = new LocalDate[dates.length];
+            int j = 0;
+            for (Date t2 : dates){
+                LocalDate dt2 = convertToLocalDateViaInstant(t2);
+                datatime[j] = dt2;
+                j++;
+            }
+            x2 = DateColumn.create("date",  datatime);
+        }
         DoubleColumn yO = DoubleColumn.create("open value", open);
         DoubleColumn yH = DoubleColumn.create("max value", high);
         DoubleColumn yL = DoubleColumn.create("min value", low);
         DoubleColumn yC = DoubleColumn.create("close value", close);
         Table ohlc = Table.create("Wykres OHLC");
-        ohlc.addColumns(x2, yO, yH, yL, yC);
-        Plot.show(CandlestickPlot.create("Candlestick Plot", ohlc, "date", "open value", "max value", "min value", "close value" ));
+        ohlc.addColumns(days == 1? x1:x2 , yO, yH, yL, yC);
 
+        Trace trace;
+        Layout layout;
+        if (days == 1){
+            trace =  ScatterTrace.builder(x1, yO, yH, yL, yC)
+                    .increasing(Increasing.builder().build())
+                    .decreasing(Decreasing.builder().build())
+                    .whiskerWidth(0.5)
+                    .type("candlestick")
+                    .build();
+        }
+        else {
+            Table sum1 = ohlc.summarize("max value", max).by("date");
+            Table sum2 = ohlc.summarize("min value", min).by("date");
+            Table open1 = ohlc.summarize("open value", first).by("date");
+            Table close1 = ohlc.summarize("close value", last).by("date");
+
+            trace =  ScatterTrace.builder(sum1.column("date"), (DoubleColumn) open1.column("First [open value]"), (DoubleColumn) sum1.column("Max [max value]"), (DoubleColumn) sum2.column("Min [min value]"), (DoubleColumn) close1.column("Last [close value]"))
+                    .increasing(Increasing.builder().build())
+                    .decreasing(Decreasing.builder().build())
+                    .whiskerWidth(0.5)
+                    .mode(ScatterTrace.Mode.MARKERS)
+                    .showLegend(false)
+                    .type("candlestick")
+                    .build();
+        }
+
+        String day = days > 1 ? "days" : "day";
+        layout = Layout.builder()
+                .title(id.toUpperCase() + " price for the last " + days + " " + day)
+                .height(650)
+                .width(800)
+                .build();
+
+        Plot.show(new Figure(layout, trace));
     }
 
-    public static void main(String[] args) throws ParseException {
-        Plots plots = new Plots();
-        APIClient api = new APIClient();
-        plots.ExchangeRatePlot("bitcoin", "usd", 7);
-        plots.ExchangeRateDiffPlot("bitcoin", "usd", 7);
-        plots.CandleStickPlot("bitcoin", "usd", 14);
-
-
-
+    public static String formatNumber(long n){
+        NumberFormat numFormat;
+        numFormat = new DecimalFormat("0.####E0");
+        String number = numFormat.format(n).replace("E", " x 10^");
+        return number;
     }
+
 }
